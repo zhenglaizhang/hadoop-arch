c995441567888eb3f3d1d54dff06ce5db7ae9001
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-02 13:42:53 +0800

    Add more tests and demos about Avro

--- src/test/java/net/zhenglai/test/AvroTest.java
+++ src/test/java/net/zhenglai/test/AvroTest.java
@@ -20,8 +20,7 @@ import java.io.File;
 import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  * Created by Zhenglai on 8/2/16.
@@ -77,12 +76,36 @@ public class AvroTest {
         encoder.flush();
         out.close();
 
+        // add one filed with default value should be ok for reader schema evaluation
+        Schema.Parser parser2 = new Schema.Parser();
+        Schema newSchema = parser2.parse("{\n" +
+                "\"type\": \"record\",\n" +
+                "\"name\": \"StringPair\",\n" +
+                "\"doc\": \"A pair of strings with an added field.\",\n" +
+                "\"fields\": [\n" +
+                "{\"name\": \"first\", \"type\": \"string\", \"aliases\":[\"left\"]},\n" +
+                "{\"name\": \"right\", \"type\": \"string\"},\n" +
+                "{\"name\": \"description\", \"type\": \"string\", \"default\": \"\"}\n" +
+                "]\n" +
+                "}");
+        // to make the default value null rather than the empty string, use:
+        // {"name": "description", "type": ["null", "string"], "default": null}
 
-        DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
+        /*
+
+        For datafiles, which have the writer’s schema stored in the metadata, we only need to
+specify the reader’s schema explicitly, which we can do by passing null for the writer’s
+schema:
+         */
+        DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema, newSchema);
         Decoder decoder = DecoderFactory.get().binaryDecoder(out.toByteArray(), null);
         GenericRecord result = reader.read(null, decoder);
-        assertThat(result.get("left").toString(), is("L"));
+        assertThat(result.get("first").toString(), is("L"));
+
+        // aliases are used to translate (at read time) the writer’s schema into the reader’s, but the alias names are not available to the reader.
+        assertEquals(result.get("left"), null);
         assertThat(result.get("right").toString(), is("R"));
+        assertThat(result.get("description").toString(), is(""));
     }
 
     /*
@@ -116,16 +139,17 @@ $ mvn -q exec:java -Dexec.mainClass=example.SpecificMain
                 .setRight("Hello")
                 .build();
         // Serialize user1, user2 and user3 to disk
+        File file = new File("target/StringPair.avro");
         DatumWriter<StringPair> userDatumWriter = new SpecificDatumWriter<StringPair>(StringPair.class);
         DataFileWriter<StringPair> dataFileWriter = new DataFileWriter<StringPair>(userDatumWriter);
-        dataFileWriter.create(datum.getSchema(), new File("users.avro"));
+        dataFileWriter.create(datum.getSchema(), file);
         dataFileWriter.append(datum);
         dataFileWriter.append(sp1);
         dataFileWriter.append(sp2);
         dataFileWriter.close();
 
         // Deserialize Users from disk
-        File file = new File("users.avro");
+        // we don’t have to specify a schema, since it is read from the file metadata
         DatumReader<StringPair> userDatumReader = new SpecificDatumReader<StringPair>(StringPair.class);
         DataFileReader<StringPair> dataFileReader = new DataFileReader<StringPair>(file, userDatumReader);
         StringPair user = null;
