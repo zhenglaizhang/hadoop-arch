cf89c3761f3914bac150ea77594e22eedcf533c4
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-08-02 11:29:37 +0800

    Add Avro specific api test

--- src/test/java/net/zhenglai/test/AvroTest.java
+++ src/test/java/net/zhenglai/test/AvroTest.java
@@ -1,12 +1,15 @@
 package net.zhenglai.test;
 
 
+import net.zhenglai.avro.StringPair;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.*;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -77,5 +80,26 @@ public class AvroTest {
         GenericRecord result = reader.read(null, decoder);
         assertThat(result.get("left").toString(), is("L"));
         assertThat(result.get("right").toString(), is("R"));
+    }
+
+    @Test
+    public void testSpecificApi() throws IOException {
+        StringPair datum = new StringPair();
+        datum.setLeft("L");
+        datum.setRight("R");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DatumWriter<StringPair> writer =
+                new SpecificDatumWriter<StringPair>(StringPair.class);
+        Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        writer.write(datum, encoder);
+        encoder.flush();
+        out.close();
+        DatumReader<StringPair> reader =
+                new SpecificDatumReader<StringPair>(StringPair.class);
+        Decoder decoder = DecoderFactory.get().binaryDecoder(out.toByteArray(),
+                null);
+        StringPair result = reader.read(null, decoder);
+        assertThat(result.getLeft(), is("L"));
+        assertThat(result.getRight(), is("R"));
     }
 }
